6b7855ceb9d3fb8e6e00eb77ce745137fc9be699
Your Name <arminpp1379@gmail.com>
2020-11-23 22:20:04 +0330

    A 2-to-4 Decoder

---
+++ 2-to-4 Decoder.v
@@ -0,0 +1,15 @@
+//////////An 2-to-4 Decoder
+
+`timescale 10ns / 10ps
+module Decoder(in,En,out);
+input wire [1:0]in;
+input wire En;
+output wire [3:0]out;
+
+assign out[0] = ~in[0] & ~in[1] & En;
+assign out[1] = in[0] & ~in[1] & En;
+assign out[2] = ~in[0] & in[1] & En;
+assign out[3] = in[0] & in[1] & En;
+  
+
+endmodule
