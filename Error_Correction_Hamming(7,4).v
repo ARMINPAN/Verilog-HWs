3a9cf19aa101b08743183badebce79f3fff468b6
Your Name <arminpp1379@gmail.com>
2020-11-23 21:11:03 +0330

    Error_Correction_Haming(7,4)

--- Error_Correction_Hamming(7,4).v
+++ Error_Correction_Hamming(7,4).v
@@ -1,8 +1,9 @@
+
 `timescale 10ns / 10ps
-module hamming(in,out);
+module hamming(in,out,paritycheckers);
 input wire [0:6]in;
 output reg [3:0]out;
-reg [3:0]paritycheckers;
+output reg [2:0]paritycheckers;
 
 always@(in)
     begin
@@ -11,10 +12,10 @@ always@(in)
         paritycheckers[2] = in[6]^in[5]^in[4]^in[3];
         ///////////////////////////////////////
         case(paritycheckers)
-            3'b111: out = {~in[2], in[4], in[5], in[6]};
-            3'b110: out = {in[2], ~in[4], in[5], in[6]};
-            3'b101: out = {in[2], in[4], ~in[5], in[6]};
-            3'b011: out = {in[2], in[4],  in[5], ~in[6]};
+            3'b111: out = {in[2], in[4], in[5], ~in[6]};
+            3'b110: out = {in[2], in[4], ~in[5], in[6]};
+            3'b101: out = {in[2], ~in[4], in[5], in[6]};
+            3'b011: out = {~in[2], in[4],  in[5], in[6]};
             default: out = {in[2], in[4], in[5], in[6]};
         endcase
     end
