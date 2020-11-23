5bf857a5709356c20b332435651fd8caf3e8f462
Your Name <arminpp1379@gmail.com>
2020-11-23 13:42:51 +0330

    An 8-bits Two`s Complement

--- Two`s complement.v
+++ Two`s complement.v
@@ -1,14 +1,10 @@
 `timescale 1ns / 1ps
-module second_complement(in,out);
+module two_complement(in,out);
   input wire [7:0]in;
   output wire [7:0]out;
-  integer i;
 
+  assign out = ~in + 1;
 
-  for ( i = 0 ; i < 7 ; i = i + 1)
-    assign out[i] = ~in[i];
-
-  assign out = out + 8'b00000001;
-
+end module
 
 
