a955da46c022bf2e46f709f0bf4eebe3628614c0
Your Name <arminpp1379@gmail.com>
2020-11-24 00:05:32 +0330

    A 2-to-1 MUX

---
+++ 2-to-1 Multiplexer.v
@@ -0,0 +1,9 @@
+////////////2-to-1 Multiplexer
+`timescale 10ns / 10ps
+module MUX(in,selector,out);
+input wire [1:0]in;
+input wire selector;
+output wire out;
+
+assign out = ~selector&in[0] + selector&in[1];
+endmodule
