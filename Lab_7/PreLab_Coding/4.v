b57bb16f41e7ffdecab258827126bdb5966a94fd
Your Name <arminpp1379@gmail.com>
2020-12-23 02:23:34 +0330

    some debuggings

--- Lab_7/PreLab_Coding/4.v
+++ Lab_7/PreLab_Coding/4.v
@@ -8,7 +8,7 @@
 //e     c//
 //   d   //
 module TrafficLightCounter(CLK,a1,b1,c1,d1,e1,f1,g1,a2,b2,c2,d2,e2,f2,g2);
-    input wire CLK;s
+    input wire CLK;
     output reg a1,b1,c1,d1,e1,f1,g1,a2,b2,c2,d2,e2,f2,g2;
     reg [3:0]hundreds = 4'b0001;
     reg [3:0]unity = 4'b0010;
@@ -52,6 +52,30 @@ module TrafficLightCounter(CLK,a1,b1,c1,d1,e1,f1,g1,a2,b2,c2,d2,e2,f2,g2);
                             unity = 4'b0010;
                         end    
                 end
+            case(hundreds)
+                4'b0000: {a1,b1,c1,d1,e1,f1,g1}=7'b1111110;
+                4'b0001: {a1,b1,c1,d1,e1,f1,g1}=7'b0110000;
+                4'b0010: {a1,b1,c1,d1,e1,f1,g1}=7'b1101101;
+                4'b0011: {a1,b1,c1,d1,e1,f1,g1}=7'b1111001;
+                4'b0100: {a1,b1,c1,d1,e1,f1,g1}=7'b0110011;
+                4'b0101: {a1,b1,c1,d1,e1,f1,g1}=7'b1011011;
+                4'b0110: {a1,b1,c1,d1,e1,f1,g1}=7'b1011111;
+                4'b0111: {a1,b1,c1,d1,e1,f1,g1}=7'b1110000;
+                4'b1000: {a1,b1,c1,d1,e1,f1,g1}=7'b1111111;
+                4'b1001: {a1,b1,c1,d1,e1,f1,g1}=7'b1110011;
+            endcase
+            case(unity)
+                4'b0000: {a2,b2,c2,d2,e2,f2,g2}=7'b1111110;
+                4'b0001: {a2,b2,c2,d2,e2,f2,g2}=7'b0110000;
+                4'b0010: {a2,b2,c2,d2,e2,f2,g2}=7'b1101101;
+                4'b0011: {a2,b2,c2,d2,e2,f2,g2}=7'b1111001;
+                4'b0100: {a2,b2,c2,d2,e2,f2,g2}=7'b0110011;
+                4'b0101: {a2,b2,c2,d2,e2,f2,g2}=7'b1011011;
+                4'b0110: {a2,b2,c2,d2,e2,f2,g2}=7'b1011111;
+                4'b0111: {a2,b2,c2,d2,e2,f2,g2}=7'b1110000;
+                4'b1000: {a2,b2,c2,d2,e2,f2,g2}=7'b1111111;
+                4'b1001: {a2,b2,c2,d2,e2,f2,g2}=7'b1110011;
+            endcase
         end
 
 
