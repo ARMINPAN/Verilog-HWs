2083daa696e9b79605b2b64e9c0fb91ff5c77081
Your Name <arminpp1379@gmail.com>
2020-12-23 03:43:49 +0330

    some debuggings at borders and PreLab7 is finished

--- Lab_7/PreLab_Coding/4.v
+++ Lab_7/PreLab_Coding/4.v
@@ -10,49 +10,81 @@
 module TrafficLightCounter(CLK,a1,b1,c1,d1,e1,f1,g1,a2,b2,c2,d2,e2,f2,g2);
     input wire CLK;
     output reg a1,b1,c1,d1,e1,f1,g1,a2,b2,c2,d2,e2,f2,g2;
-    reg [3:0]hundreds = 4'b0001;
-    reg [3:0]unity = 4'b0010;
-    reg flag = 1'b1;
+    reg [3:0]hundreds;
+    reg [3:0]unity;
+    reg flag = 1'b0;
+    reg flag2 = 1'b1;
+    reg atzero = 1'b1;
 
     always@(posedge CLK)
         begin
+            if(flag2 == 1'b1)
+                begin
+                    hundreds = 4'b0001;
+                    unity = 4'b0010;
+                    flag2 = 1'b0;
+                end
             if(flag == 1'b0)
                 begin
-                    if(unity != 0)
+                    if(unity != 4'b0000)
                         begin
                             unity = unity - 4'b0001;
                         end
-                    if(unity == 0 && hundreds !=0)
+                    if(unity == 0 && hundreds != 4'b0000)
                         begin
-                            unity = 4'b1001;
-                            hundreds = hundreds - 4'b0001;
+                            if(atzero == 1'b0)                            
+                                begin
+                                            unity = 4'b1001;
+                                            hundreds = hundreds - 4'b0001;
+                                             atzero = 1'b1;
+                                end
+                            else
+                                atzero = 1'b0;
                         end
-                    if(unity == 0 && hundreds ==0)    
+                    if(unity == 4'b0000 && hundreds == 4'b0000)    
                         begin
-                            flag = 1'b1;
-                            hundreds = 4'b1000;
-                            unity = 4'b1000;
-                        end        
+                            if(atzero == 1'b0)
+                                begin
+                                    hundreds = 4'b1000;
+                                    unity = 4'b1000;
+                                    flag = 1'b0;
+                                    atzero = 1'b1;
+                                end   
+                            else
+                                atzero = 1'b0;
+                        end  
                 end
             if(flag == 1'b1)
                 begin
-                    if(unity != 0)
+                    if(unity != 4'b0000)
                         begin
                             unity = unity - 4'b0001;
                         end
-                    if(unity == 0 && hundreds !=0)
+                    if(unity == 0 && hundreds != 4'b0000)
                         begin
-                            unity = 4'b1001;
-                            hundreds = hundreds - 4'b0001;
+                            if(atzero == 1'b0)                            
+                                begin
+                                            unity = 4'b1001;
+                                            hundreds = hundreds - 4'b0001;
+                                             atzero = 1'b1;
+                                end
+                            else
+                                atzero = 1'b0;
                         end
-                    if(unity == 0 && hundreds ==0)    
+                    if(unity == 4'b0000 && hundreds == 4'b0000)    
                         begin
-                            flag = 1'b0;
-                            hundreds = 4'b0001;
-                            unity = 4'b0010;
-                        end    
+                            if(atzero == 1'b0)
+                                begin
+                                                    hundreds = 4'b0001;
+                                                    unity = 4'b0010;
+                                                    flag = 1'b0;
+                                                    atzero = 1'b1;
+                                end   
+                            else
+                                atzero = 1'b0;
+                        end  
                 end
-            case(hundreds)
+            case(unity)
                 4'b0000: {a1,b1,c1,d1,e1,f1,g1}=7'b1111110;
                 4'b0001: {a1,b1,c1,d1,e1,f1,g1}=7'b0110000;
                 4'b0010: {a1,b1,c1,d1,e1,f1,g1}=7'b1101101;
@@ -64,7 +96,7 @@ module TrafficLightCounter(CLK,a1,b1,c1,d1,e1,f1,g1,a2,b2,c2,d2,e2,f2,g2);
                 4'b1000: {a1,b1,c1,d1,e1,f1,g1}=7'b1111111;
                 4'b1001: {a1,b1,c1,d1,e1,f1,g1}=7'b1110011;
             endcase
-            case(unity)
+            case(hundreds)
                 4'b0000: {a2,b2,c2,d2,e2,f2,g2}=7'b1111110;
                 4'b0001: {a2,b2,c2,d2,e2,f2,g2}=7'b0110000;
                 4'b0010: {a2,b2,c2,d2,e2,f2,g2}=7'b1101101;
